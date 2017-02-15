b420c22fddc5c978af28e90dc4f5367c57f740a5
pascalumaknow <pascal@umaknow.com>
2017-02-15 15:48:39 -0500

    Working with everything including appropriate IP ranges

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -10,3 +10,4 @@
 $rgName = "gem" + [guid]::NewGuid()
 New-AzureRmResourceGroup -Name $rgName -Location "West US"
 New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
+ 
