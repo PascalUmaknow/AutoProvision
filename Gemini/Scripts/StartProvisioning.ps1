505124cb945ff6f59292e77626fb1f8e53be6073
pascalumaknow <pascal@umaknow.com>
2017-02-15 13:03:29 -0500

    working with load balancers

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -6,7 +6,7 @@
 #Test-AzureRmResourceGroupDeployment -ResourceGroupName "testgem" -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
 #exit
  
-
+  
 $rgName = "gem" + [guid]::NewGuid()
 New-AzureRmResourceGroup -Name $rgName -Location "West US"
 New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
