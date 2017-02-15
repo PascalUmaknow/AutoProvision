c0fe829c37012d29d180c861bc707db982138157
pascalumaknow <pascal@umaknow.com>
2017-02-15 12:37:50 -0500

    f

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -3,8 +3,8 @@
 
 
 #TEST
-Test-AzureRmResourceGroupDeployment -ResourceGroupName "testgem" -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
-exit
+#Test-AzureRmResourceGroupDeployment -ResourceGroupName "testgem" -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
+#exit
  
 
 $rgName = "gem" + [guid]::NewGuid()
