7a02f779a27a4c32911429711e9f6355a0879ab7
pascalumaknow <pascal@umaknow.com>
2017-02-24 11:32:03 -0500

    update VM Image to windows server 2012 R2

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -7,7 +7,13 @@ Login-AzureRmAccount
 #exit
  
   
-$rgName = "gem" + [guid]::NewGuid()
-New-AzureRmResourceGroup -Name $rgName -Location "West US"
+$rgName = "GeminiPOC2"
+#New-AzureRmResourceGroup -Name $rgName -Location "East US"
 New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
  
+ 
+ 
+#$locName="East US"
+#Get-AzureRMVMImagePublisher -Location $locName | Select PublisherName
+#Get-AzureRMVMImageOffer -Location "East US"  -Publisher "MicrosoftWindowsServer" | Select Offer
+#Get-AzureRMVMImageSku -Location "East US" -Publisher "MicrosoftWindowsServer" -Offer "WindowsServer" | Select Skus
