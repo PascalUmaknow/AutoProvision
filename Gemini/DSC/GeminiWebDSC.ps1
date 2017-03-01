36a0c5b73f431f78678b92d3d9f77bef4282d6a4
pascalumaknow <pascal@umaknow.com>
2017-03-01 11:19:35 -0500

    Add DSC Files

---
+++ Gemini/DSC/GeminiWebDSC.ps1
@@ -0,0 +1,27 @@
+#
+# GeminiWebDSC.ps1
+#
+Configuration ContosoWebsite
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+   
+    #Install the IIS Role 
+    WindowsFeature IIS 
+    { 
+      Ensure = “Present” 
+      Name = “Web-Server” 
+    }
+  WindowsFeature NetFramework35Core {
+Name = “NET-Framework-Core”
+Ensure = “Present”
+}
+
+WindowsFeature NetFramework45Core {
+Name = “NET-Framework-45-Core”
+Ensure = “Present”
+} 
+  }
+} 
