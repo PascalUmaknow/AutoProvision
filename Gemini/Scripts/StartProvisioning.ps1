1183b91bcf0ee3ea69457b9bee2671ee1073a6e2
pascalumaknow <pascal@umaknow.com>
2017-03-08 09:42:26 -0500

    try scalesets 1

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -20,7 +20,7 @@ New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $r
 # New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://geminipocarmtemplates.blob.core.windows.net/geminipocarmtemplate/azuredeploy.json?st=2017-03-01T15%3A16%3A00Z&se=2018-03-02T15%3A16%3A00Z&sp=rl&sv=2015-12-11&sr=b&sig=nxxEvd2E274XdthViL0iZjH6EWqsTw3aLBtuKyIiUZY%3D" -Verbose -_artifactsLocation "https://geminipocarmtemplates.blob.core.windows.net/geminipocarmtemplate" -_artifactsLocationSasToken  $token
 
  #cleanup a RG
-#  New-AzureRmResourceGroupDeployment -ResourceGroupName "GeminiPOC1" -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
+  New-AzureRmResourceGroupDeployment -ResourceGroupName "GeminiPOC1" -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
  
 #$locName="East US"
 #Get-AzureRMVMImagePublisher -Location $locName | Select PublisherName
