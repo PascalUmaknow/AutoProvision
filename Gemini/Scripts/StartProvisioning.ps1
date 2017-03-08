2e94ee1cbaac6c45de117d988a7fdd20bdbe983f
pascalumaknow <pascal@umaknow.com>
2017-03-08 09:48:17 -0500

    f

--- Gemini/Scripts/StartProvisioning.ps1
+++ Gemini/Scripts/StartProvisioning.ps1
@@ -2,14 +2,17 @@
 
 
 
-#TEST
-#Test-AzureRmResourceGroupDeployment -ResourceGroupName "testgem" -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
-#exit
+
  
   
 $rgName = "GeminiPOC1"
+
+#TEST
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rgName -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json"
+#exit
+
 #New-AzureRmResourceGroup -Name $rgName -Location "East US"
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json" -Verbose 
+#New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/azuredeploy.json" -Verbose 
  
  
  #secured deployment
@@ -20,7 +23,7 @@ New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $r
 # New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://geminipocarmtemplates.blob.core.windows.net/geminipocarmtemplate/azuredeploy.json?st=2017-03-01T15%3A16%3A00Z&se=2018-03-02T15%3A16%3A00Z&sp=rl&sv=2015-12-11&sr=b&sig=nxxEvd2E274XdthViL0iZjH6EWqsTw3aLBtuKyIiUZY%3D" -Verbose -_artifactsLocation "https://geminipocarmtemplates.blob.core.windows.net/geminipocarmtemplate" -_artifactsLocationSasToken  $token
 
  #cleanup a RG
-  New-AzureRmResourceGroupDeployment -ResourceGroupName "GeminiPOC1" -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
+ # New-AzureRmResourceGroupDeployment -ResourceGroupName "GeminiPOC1" -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
  
 #$locName="East US"
 #Get-AzureRMVMImagePublisher -Location $locName | Select PublisherName
