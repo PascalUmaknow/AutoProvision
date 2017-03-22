66379ab101b83302e6a9530c13200a2fc18ed5df
pascalumaknow <pascal@umaknow.com>
2017-03-22 13:36:33 -0400

    add VPN Gateway template

--- Gemini/Scripts/ProvisionVPNGateway.ps1
+++ Gemini/Scripts/ProvisionVPNGateway.ps1
@@ -2,8 +2,11 @@
   
 $rgName = "GeminiPOC2"
 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
+ 
+
 $refreshFile=[system.guid]::newguid().ToString()
  
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -TemplateUri ("https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/VPNGatewayTemplates/azuredeploy.json?test="+$refreshFile) -TemplateParameterUri  ("https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/VPNGatewayTemplates/azuredeploy.parameters.json?test="+$refreshFile) -Verbose
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -TemplateFile "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.json" -TemplateParameterFile  "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.parameters.json" -Verbose
 
  
