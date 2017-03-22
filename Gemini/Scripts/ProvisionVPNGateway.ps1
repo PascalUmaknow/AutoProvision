4a4dd86e4e509324936c2d3768bb9a9a973e4fe4
pascalumaknow <pascal@umaknow.com>
2017-03-22 12:46:14 -0400

    add geminivnet reference

--- Gemini/Scripts/ProvisionVPNGateway.ps1
+++ Gemini/Scripts/ProvisionVPNGateway.ps1
@@ -1,9 +1,9 @@
-Login-AzureRmAccount
+#Login-AzureRmAccount
   
 $rgName = "GeminiPOC2"
 
 $refreshFile=[system.guid]::newguid().ToString()
  
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -TemplateUri ("https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/VPNGatewayTemplates/azuredeploy.json?test="+$refreshFile) -Verbose
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -TemplateUri ("https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/VPNGatewayTemplates/azuredeploy.json?test="+$refreshFile) -TemplateParameterUri  ("https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/VPNGatewayTemplates/azuredeploy.parameters.json?test="+$refreshFile) -Verbose
 
  
