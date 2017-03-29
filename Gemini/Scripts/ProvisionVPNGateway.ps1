aac928f87faf302843ab5d992856b044aa850176
pascalumaknow <pascal@umaknow.com>
2017-03-29 10:30:30 -0400

    update VPN script to insert into an existing subnet.

--- Gemini/Scripts/ProvisionVPNGateway.ps1
+++ Gemini/Scripts/ProvisionVPNGateway.ps1
@@ -1,12 +1,12 @@
 #Login-AzureRmAccount
   
-$rgName = "GeminiPOC2"
+$rgName = "GeminiPOC3"
 
 #New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName  -Mode Complete -TemplateUri "https://raw.githubusercontent.com/PascalUmaknow/AutoProvision/master/Gemini/Templates/cleanup/ResourceGroupCleanup.template.json" -Force -Verbose  
  
 
 #$refreshFile=[system.guid]::newguid().ToString()
  
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName $rgName  -TemplateFile "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.json" -TemplateParameterFile  "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.parameters.json" -Verbose
+New-AzureRmResourceGroupDeployment -Name VPNDeployment -ResourceGroupName $rgName  -TemplateFile "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.json" -TemplateParameterFile  "C:\Users\Pascal\Desktop\CAE ARM Templates\Gemini\VPNGatewayTemplates\azuredeploy.parameters.json" -Verbose
 
  
